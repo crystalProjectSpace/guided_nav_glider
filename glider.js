7390eea798278a0941677ac05d799ec046c816b9
crystalProjectSpace <sevmailbox@yandex.ru>
2023-03-01 23:59:00 +0300

    use pre-defined coord for guidance to avoid array creation/destruction overheads

---
+++ glider.js
@@ -0,0 +1,117 @@
+'use strict'
+
+const {
+	linterp,
+	createAlphaFunc,
+	createGammaFunc,
+	createObserver,
+	integrate,
+} = require('./utils.js')
+
+const { Vect3D } = require('./vect3.js')
+
+class Glider {
+	constructor(m, S, CXA_vect, CYA_vect, AOA_vect) {
+		this.params = {
+			S,
+			m,
+			Cxa: function(alpha) {
+				return linterp(CXA_vect, AOA_vect, alpha)
+			},
+			Cya: function(alpha) {
+				return linterp(CYA_vect, AOA_vect, alpha)
+			},
+		}
+		
+		this.controls = {
+			AoA: null,
+			roll: null,
+		}
+		
+		this.observe = null
+		
+		this.targeting = {
+			visActive: null,
+			visPrev: null,
+			dVisAngle: 0,
+			distance: 1E10,
+			ascend: 0,
+			alpha: 0,
+			gamma: 0,
+			dive: 0,
+		}
+	}
+	
+	setPitchControl(pitchFunction) {
+		this.controls.AoA = pitchFunction
+	}
+	
+	setRollControl(rollFunction) {
+		this.controls.roll = rollFunction
+	}
+	
+	setObserver(observeFunction, ctrlPoint) {
+		this.observe = observeFunction(this.targeting, ctrlPoint)
+	}
+	
+	getTrajectory(initialState, flag, dT) {
+		return integrate(
+			initialState,
+			this.params,
+			this.controls,
+			this.observe,
+			flag,
+			dT,
+		)
+	}
+}
+
+const defaultFlightEnd = function(state, t) {
+	return state[4] < 0
+}
+
+const Cxa_glider = [0.9466,	0.4346,	0.2554,	0.1274,	0.0905, 0.0619, 0.0414, 0.0291, 0.0250, 0.0291, 0.0414, 0.0619, 0.0905, 0.1274, 0.2554, 0.4346, 0.9466]
+const Cya_glider = [-2.4000, -1.6000, -1.2000, -0.8000, -0.6400, -0.4800, -0.3200, -0.1600, 0.0000, 0.1600, 0.3200, 0.4800, 0.6400, 0.8000, 1.2000, 1.6000, 2.4000]
+const AoA_glider = [-30,		-20,	-15,		-10,	-8,		-6,		-4,		- 2,		0,	2,		4,		6,		8,		10,		15,		20,		30]
+const M0 = 75
+const S0 = 0.5
+const dT_base = 0.025
+
+const INIT_STATE = [485, 5/57.3, 0, 0, 1250, 0]
+
+const ALPHA_CTRL_PRM = {
+	alphaBase: 2.15,
+	kTh: 0.95,
+	alphaDive: -3.5,
+}
+
+const GAMMA_CTRL_PRM = {
+	rollStart: 2.5,
+	rollEnd: 125,
+	rollPerSecond: 35  * 0.1 / 57.3,
+}
+
+const testGlider = new Glider(M0, S0, Cxa_glider, Cya_glider, AoA_glider)
+testGlider.setPitchControl(createAlphaFunc(ALPHA_CTRL_PRM, testGlider))
+testGlider.setRollControl(createGammaFunc(GAMMA_CTRL_PRM, testGlider))
+testGlider.setObserver(createObserver, Vect3D.fromXYZ(8000, 0, -1500))
+
+const gliderTrajectory = testGlider.getTrajectory(INIT_STATE, defaultFlightEnd, dT_base)
+
+const response = gliderTrajectory.reduce((res, row, i) => {
+	const { state, t } = row
+	if (i % 5 !== 0) return res
+	res += [
+		t.toFixed(1),
+		state[0].toFixed(1),
+		(state[1] * 57.3).toFixed(2),
+		(state[2] * 57.3).toFixed(2),
+		state[3].toFixed(0),
+		state[4].toFixed(0),
+		state[5].toFixed(0),
+	].join('\t') + '\n'
+	
+	return res
+}, '')
+
+console.log(response)
---
+++ utils.js
@@ -0,0 +1,179 @@
+'use strict'
+const { Vect3D, Plane } = require('./vect3.js')
+// adx for small sub/transsonic drone with long thin wing
+// cxa0 = 0.025, Apolar = 0.16, dCdY = 0.08
+const ALPHA_SAFE = 22.5
+const g = 9.81
+
+const GROUND_PLANE = new Plane(
+	Vect3D.fromXYZ(0, 0, 0),
+	Vect3D.fromXYZ(0, 1, 0),
+)
+
+const linterp = function(YV, XV, X) {
+	let i = 0
+	while(XV[i + 1] < X) { i++ }
+	const i1 = i + 1
+	
+	return YV[i] + (YV[i1] - YV[i]) * (X - XV[i]) / (XV[i1] - XV[i])
+}
+
+const getRo = function(H) {
+	return 1.2 * Math.exp(-0.0001261 * H)
+}
+
+const derivatives = function(state, params, controls, t) {
+	const alpha = controls.AoA(state, t)
+	const gamma = controls.roll(state, t)
+	
+	const V = state[0]
+	const Th = state[1]
+	const Psi = state[2]
+	const H = state[4]
+	const CTH = Math.cos(Th)
+	const STH = Math.sin(Th)
+	
+	const CPSI = Math.cos(Psi)
+	const SPSI = Math.sin(Psi)
+	
+	const CG = Math.cos(gamma)
+	const SG = Math.sin(gamma)
+	
+	const Ro = getRo(H)
+		
+	const QS = 0.5 * Ro * V * V * params.S
+
+	const Xa = params.Cxa(alpha) * QS
+	const Ya = params.Cya(alpha) * QS
+	const MV = params.m * V
+		
+	return [
+		(- params.m * g * STH - Xa) / params.m,
+		(Ya * CG - params.m * g * CTH) / MV,
+		Ya * SG / MV,
+		V * CTH * CPSI,
+		V * STH,
+		V * CTH * SPSI,
+	]
+}
+
+const integrate = function(initialState, params, controls, observe, flag, dT) {
+	const result = [{t: 0, state: initialState.slice()}]
+	const K_buffer = [0, 0, 0, 0, 0, 0]
+	const dT_05 = 0.5 * dT
+	let i = 0
+	let tau = 0
+	
+	while(!flag(result[i].state, tau)) {
+		const activeState = result[i].state
+		observe(activeState, dT)
+		
+		const deriv_0 = derivatives(activeState, params, controls, tau)
+
+		K_buffer[0] = activeState[0] + deriv_0[0] * dT
+		K_buffer[1] = activeState[1] + deriv_0[1] * dT
+		K_buffer[2] = activeState[2] + deriv_0[2] * dT
+		K_buffer[3] = activeState[3] + deriv_0[3] * dT
+		K_buffer[4] = activeState[4] + deriv_0[4] * dT
+		K_buffer[5] = activeState[5] + deriv_0[5] * dT
+
+		const deriv_1 = derivatives(K_buffer, params, controls, tau)
+		tau += dT
+		i++
+		result.push({
+			state: [
+				activeState[0] + (deriv_0[0] + deriv_1[0]) * dT_05,
+				activeState[1] + (deriv_0[1] + deriv_1[1]) * dT_05,
+				activeState[2] + (deriv_0[2] + deriv_1[2]) * dT_05,
+				activeState[3] + (deriv_0[3] + deriv_1[3]) * dT_05,
+				activeState[4] + (deriv_0[4] + deriv_1[4]) * dT_05,
+				activeState[5] + (deriv_0[5] + deriv_1[5]) * dT_05,
+			],
+			t: tau
+		})
+	}
+	
+	return result
+}
+
+const createObserver = function(targeting, ctrlPoint) {
+	const coords = Vect3D.fromXYZ(0, 0, 0)
+	const VECT = Vect3D.fromXYZ(0, 0, 0)
+
+	return function(state, dT) {
+		const CTH = Math.cos(state[1])
+		const STH = Math.sin(state[1])
+		const CPSI = Math.cos(state[2])
+		const SPSI = Math.sin(state[2])
+
+		coords[0] = state[3]
+		coords[1] = state[4]
+		coords[2] = state[4]
+
+		VECT[0] = CTH * CPSI
+		VECT[1] = STH 
+		VECT[2] = CTH * SPSI
+		
+		const cr_projection = GROUND_PLANE.projectPoint(coords)
+		const v_projection = GROUND_PLANE.projectPoint(VECT)
+		const delta_cr = coords.subt(ctrlPoint)
+
+		targeting.visPrev = targeting.visActive?.clone()
+		targeting.visActive = ctrlPoint.subt(cr_projection)
+		
+		const ascend = v_projection.angle(VECT)
+		targeting.dVisAngle = targeting.visPrev ? (targeting.visActive.angle(targeting.visPrev)) / dT : 0
+		targeting.ascend = ascend
+		targeting.distance = delta_cr.abs()
+	}
+}
+
+ const createAlphaFunc = function(ctrlParams, vehicle) {
+	const { alphaBase, kTh } = ctrlParams
+	return function(state, t) {
+		if(t < 2.5) return 0
+		const { targeting } = vehicle
+		if(targeting.distance > 2000) {
+			const alpha = alphaBase - (state[1] * 57.3) * kTh
+			if(alpha < -ALPHA_SAFE) return -ALPHA_SAFE
+			if(alpha > ALPHA_SAFE) return ALPHA_SAFE
+			return alpha
+		}
+		if(targeting.distance < 2000) {
+			if(targeting.dive === 0) targeting.dive = -Math.asin(state[4]/targeting.distance)
+			const alpha = (targeting.dive - state[1]) * 6.85 * 57.3
+			if(alpha < -ALPHA_SAFE) return -ALPHA_SAFE
+			if(alpha > ALPHA_SAFE) return ALPHA_SAFE
+			return alpha
+		}
+		return 0
+	}
+}
+
+
+const createGammaFunc = function(ctrlParams, vehicle) {
+	const { rollStart, rollEnd, rollPerSecond } = ctrlParams
+	return function(state, t) {
+		if(vehicle.targeting.distance < 2000) return 0
+		if(t < rollStart) return 0
+		if(t > rollEnd) return 0
+		const { targeting } = vehicle
+		let dGamma = targeting.dVisAngle * 325.
+		if(dGamma < -rollPerSecond) dGamma = -rollPerSecond
+		if(dGamma > rollPerSecond) dGamma = rollPerSecond
+		let res = targeting.gamma + dGamma
+		if(res > 1.57) res = 1.57
+		if(res < -1.57) res = -1.57
+		targeting.gamma = res
+		//console.log(res * 57.3)
+		return targeting.gamma
+	}
+}
+
+module.exports = {
+	linterp,
+	createAlphaFunc,
+	createGammaFunc,
+	createObserver,
+	integrate,
+}
